12fff5cf58b321dd27234a4217a82955d1f0fd7d
sl fireflash <sllahiruprasanna@gmail.com>
2023-01-28 21:20:51 +0530

    gamebody basic frontend development complete

--- home.php
+++ home.php
@@ -8,9 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="bg-dark">
      <div class="wraper">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">PlayNums</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +27,7 @@
                 </ul>
                 <ul class="navbar-nav me-2 mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
+                    <a class="nav-link" aria-current="page" href="#">
                     <?php
                         session_start();
                         echo strtoupper($_SESSION['coinAmount']);
@@ -44,7 +44,7 @@
                         echo strtoupper($_SESSION['username']);
                         ?>
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Account</a></li>
                         <li><a class="dropdown-item" href="#">Settings</a></li>
                         <li><hr class="dropdown-divider"></li>
@@ -54,15 +54,51 @@
                 </ul>
             </div>
             </nav>
+            
 
-        <div class="gamebody">
+        <div class="gamebody d-flex flex-row justify-content-center">
+          <div class="select-items">
+            <div class="select-coins">
+              <h1><span>SELECT COINS</span></h1>
+                <button class="button btn-select-coin p-2" id="btn-select-coin-50"><span>50</span></button>
+                <button class="button btn-select-coin p-2" id="btn-select-coin-100"><span>100</span></button>
+                <br>
+                <button class="button btn-select-coin p-2" id="btn-select-coin-200"><span>200</span></button>
+                <button class="button btn-select-coin p-2" id="btn-select-coin-500"><span>500</span></button>
 
+            </div>
+            <div class="select-number">
+              <h1><span>SELECT NUMBER</span></h1>
+              <button class="button btn-select-number p-2"><span>1</span></button>
+              <button class="button btn-select-number p-2"><span>2</span></button>
+              <br>
+              <button class="button btn-select-number p-2"><span>3</span></button>
+              <button class="button btn-select-number p-2"><span>4</span></button>
+              <br>
+              <button class="button btn-select-number p-2"><span>5</span></button>
+              <button class="button btn-select-number p-2"><span>6</span></button>
+              <br>
+              <button class="button btn-select-number p-2"><span>7</span></button>
+              <button class="button btn-select-number p-2"><span>8</span></button>
+              <br>
+              <button class="button btn-select-number p-2"><span>9</span></button>
+              <button class="button btn-select-number p-2"><span>0</span></button>
+
+            </div>
+            <div class="start-game">
+              <h1><span>START GAME</span></h1>
+              <button class="button btn-select-number p-2"><span>RESTART</span></button>
+              <button class="button btn-select-number p-2"><span>START</span></button>
+
+            </div>
+          </div>
 
         </div>
         
+        
         <div class="footer">
             <!-- Footer -->
-      <footer class="text-center text-lg-start bg-light text-muted">
+      <footer class="text-center text-lg-start bg-dark text-muted">
         <!-- Section: Links  -->
         <section class="">
           <div class="container text-center text-md-start mt-5">
@@ -102,5 +138,5 @@
    
     <script src="js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
+
 </html>
